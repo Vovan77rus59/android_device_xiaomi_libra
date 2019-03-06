0a6b14e1d0ebee8c83ec0d1322602b201cb5b621
Владимир <vova.perm@hotmail.com>
2019-03-07 02:15:02 +0500

    libra: Go to lineage

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,14 +12,14 @@ TARGET_CPU_VARIANT := generic
 TARGET_CPU_CORTEX_A53 := true
 
 # Kernel
-TARGET_KERNEL_CONFIG := libra_user_defconfig
+TARGET_KERNEL_CONFIG := libra_defconfig
 
 # Partition
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2013265920 #1920M
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 27980184576 #26G
 
-# MK Hardware
-JAVA_SOURCE_OVERLAYS := org.mokee.hardware|$(DEVICE_PATH)/mkhw|**/*.java
+# Lineage Hardware
+JAVA_SOURCE_OVERLAYS := org.lineageos.hardware|$(DEVICE_PATH)/lineagehw|**/*.java
 
 # dt2w
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchscreen/double_tap_enable"
@@ -27,9 +27,6 @@ TARGET_TAP_TO_WAKE_NODE := "/proc/touchscreen/double_tap_enable"
 # Assertions
 TARGET_BOARD_INFO_FILE ?= $(DEVICE_PATH)/board-info.txt
 TARGET_OTA_ASSERT_DEVICE := 4C,libra
-
-# Security patch level
-VENDOR_SECURITY_PATCH := 2018-03-01
 
 # Shims
 TARGET_LD_SHIM_LIBS += /system/vendor/lib/hw/camera.vendor.msm8992.so|libshim_camera.so
---
+++ lineage_libra.mk
@@ -0,0 +1,31 @@
+# Release name
+PRODUCT_RELEASE_NAME := libra
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/xiaomi/libra/libra.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := libra
+PRODUCT_NAME := lineage_libra
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi-4c
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+TARGET_VENDOR := xiaomi
+
+LINEAGE_BUILD_TYPE := WEEKLY
+
+# Build fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="libra-user 7.0 NRD90M V10.1.1.0.NXKCNFI release-keys"
+
+BUILD_FINGERPRINT := Xiaomi/libra/libra:7.0/NRD90M/V10.1.1.0.NXKCNFI:user/release-keys
